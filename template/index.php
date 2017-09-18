d0418ee17edf7a09077f47ecc13745fb6b1fe046
N Kort <nics@limnerstudio.co.uk>
2017-09-18 17:55:19 +0100

    placeholder index

--- template/index.php
+++ template/index.php
@@ -1,2 +1,8 @@
-<?php
-  require 'src/index.php';
+<section style="padding:2em">
+  <h1>wordpress + vue</h1>
+
+  <p>
+    Navigate to the theme folder and run <code>npm install</code> and <code>npm run dev</code>.
+    This file will be overwritten during development.
+  </p>
+</section>
